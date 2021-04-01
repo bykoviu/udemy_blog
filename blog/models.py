707ae69827b9bdb620423e89acdf7a61a1d02437
bykoviu <71722112+bykoviu@users.noreply.github.com>
2021-04-01 13:17:46 +0300

    Add model Post in app Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='event_images')
+    text = models.TextField()
+    date = models.DateTimeField()
