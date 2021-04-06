429975fa86bea65b57cc6ec544858c1ad17fc6e4
bykoviu <71722112+bykoviu@users.noreply.github.com>
2021-04-06 15:39:06 +0300

    Add event's object view and and post's object view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
+def show_posts(request):
+    posts = Post.objects
+    return render(request, 'blog/blog.html', {'posts': posts})
 # Create your views here.
